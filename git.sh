3f0a8fbdb04366e97437d59fcef7132ccf13b527
IArshidinova <101781958+IArshidinova@users.noreply.github.com>
2022-06-10 13:01:02 +0500

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,7 +1,10 @@
 #!/bin/bash 
-"sudo apt-get update"
-"sudo apt-get install wget unzip"
-"sudo wget https://releases.hashicorp.com/terraform/0.12.2/terraform_0.12.2_linux_amd64.zip"
+#sudo apt-get update
+#sudo apt-get install wget unzip
 
-"sudo useradd iroda"
-"sudo yum install java-devel"
+sudo yum install -y wget zip unzip 
+wget https://releases.hashicorp.com/terraform/0.12.2/terraform_0.12.2_linux_amd64.zip
+sudo unzip terraform_0.12.2_linux_amd64.zip
+sudo cp terraform /usr/local/bin/
+terraform -version 
+
